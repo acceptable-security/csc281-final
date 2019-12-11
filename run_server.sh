e6cee49f5705940c766ca717729d166afd2cad2a
Avi Saven <block8437@gmail.com>
2019-12-11 14:23:52 -0500

    Put a small sleep to prevent races between background processes

--- run_server.sh
+++ run_server.sh
@@ -12,6 +12,8 @@ gwsocket -p 7891 \
 		 --strict &
 
 
+sleep 1
+
 cd emp-sh2pc/build/
 ./bin/editdist-socket /tmp/tmpoutA.fifo /tmp/tmpinA.fifo 12345 &
 ./bin/editdist-socket /tmp/tmpoutB.fifo /tmp/tmpinB.fifo 12345 &
