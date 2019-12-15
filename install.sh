81b66542b88cb685d2ee486dfdb84efe65454eee
Avi Saven <block8437@gmail.com>
2019-12-15 15:13:48 -0500

    Removed malicious circuits, the evil viruses of satan

--- install.sh
+++ install.sh
@@ -21,22 +21,3 @@ cd build
 cmake ..
 make
 make install
-
-#Generate mult3 circuit
-./bin/mult3 -m
-mv mult3.circuit.txt /usr/local/include/emp-tool/circuits/files/
-
-# install malicious 2-party circuits
-cd
-git clone https://github.com/emp-toolkit/emp-ag2pc.git
-
-#Fix space in error message
-sed -i 's/{cout <<ands <<"no match GT!"<<endl<<flush;/{cout <<ands <<" no match GT!"<<endl<<flush;/' ~/emp-ag2pc/emp-ag2pc/2pc.h
-
-cd emp-ag2pc
-echo "add_test (agmult3)" >> CMakeLists.txt
-mv ../emp-sh2pc/test/agmult3.cpp test/agmult3.cpp
-mkdir build
-cd build
-cmake ..
-make 
